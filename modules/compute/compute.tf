61e07148fe911b18b7330b0518366a3c9791ddbe
padfoot.black <padfoot@cybertron.io>
2023-02-05 16:43:26 -0500

    updating resource blocks in compute module

--- modules/compute/compute.tf
+++ modules/compute/compute.tf
@@ -1,11 +1,24 @@
+## Data Block for importing VPN and Subnet IDs ###
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 ### Launch Configuration for Auto Scaling Group ###
 
-resource "aws_launch_configuration" "web_launch_config" {
+resource "aws_launch_configuration" "web_launch_configuration" {
   image_id        = var.image_id
   instance_type   = var.instance_type
   security_groups = [aws_security_group.web_sg.id]
 
-  user_data = templatefile("user-data.sh", {
+  user_data = templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
   })
               
@@ -14,22 +27,10 @@ resource "aws_launch_configuration" "web_launch_config" {
   }
 }
 
-### Security Group for Launch Configuration ###
-
-resource "aws_security_group" "web_sg" {
-  name = "${var.cluster_name}-web-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
-}
-
 ### Auto Scaling Group ###
 
-resource "aws_autoscaling_group" "web_asg" {
-  launch_configuration = aws_launch_configuration.web_launch_config.name
+resource "aws_autoscaling_group" "web_autoscaling_group" {
+  launch_configuration = aws_launch_configuration.web_launch_configuration.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
 
   target_group_arns = [aws_lb_target_group.alb_target_group.arn] ##
@@ -45,31 +46,62 @@ resource "aws_autoscaling_group" "web_asg" {
   }
 }
 
+### Security Group for Launch Configuration ###
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_security_group" "web_security_group" {
+  name = "${var.cluster_name}-web-security-group"
+  
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+resource "aws_security_group_rule" "allow_web_security_group_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.web_security_group.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 ### Application Load Balancer ###
 
-resource "aws_lb" "web_alb" {
+resource "aws_lb" "web_application_loadbalancer" {
   name               = var.cluster_name
   load_balancer_type = "application"
   subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.alb_security_group.id]
+}
+
+### Security Group for Application Load Balancef ###
+
+resource "aws_security_group" "alb_security_group" {
+  name = "${var.cluster_name}-alb-security-group"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb_security_group.id
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb_security_group.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 ### Listener Rules for Application Load Balancer ###
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.web_alb.arn
+resource "aws_lb_listener" "http_alb_listener" {
+  load_balancer_arn = aws_lb.web_application_loadbalancer.arn
   port              = local.http_port
   protocol          = "HTTP"
 
@@ -85,36 +117,11 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-### Security Group for Application Load Balancef ###
-
-resource "aws_security_group" "alb_sg" {
-  name = "${var.cluster_name}-alb-sg"
-}
-
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb_sg.id
-
-  from_port   = local.http_port
-  to_port     = local.http_port
-  protocol    = local.tcp_protocol
-  cidr_blocks = local.all_ips
-}
-
-resource "aws_security_group_rule" "allow_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb_sg.id
-
-  from_port   = local.any_port
-  to_port     = local.any_port
-  protocol    = local.any_protocol
-  cidr_blocks = local.all_ips
-}
 
 ### Target Group for Application Load Balancer ###
 
-resource "aws_lb_target_group" "alb_target_group" {
-  name     = "web-alb-target-group"
+resource "aws_lb_target_group" "web_alb_target_group" {
+  name     = var.cluster_name
   port     = var.server_port
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
@@ -132,8 +139,8 @@ resource "aws_lb_target_group" "alb_target_group" {
 
 ### Attaching Listener Rule to Targer Group ###
 
-resource "aws_lb_listener_rule" "alb_listener" {
-  listener_arn = aws_lb_listener.http.arn
+resource "aws_lb_listener_rule" "web_alb_listener_rule" {
+  listener_arn = aws_lb_listener.http_alb_listener.arn
   priority     = 100
 
   condition {
@@ -144,7 +151,7 @@ resource "aws_lb_listener_rule" "alb_listener" {
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    target_group_arn = aws_lb_target_group.web_alb_target_group.arn
   }
 }
 
