08f10063d47f151e93c8f2dce06527cfdc564fc5
padfoot.black <padfoot@cybertron.io>
2023-02-05 12:35:05 -0500

    directory cleanup

--- aws_live_workloads/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "remote-state-065374699403"
-    key     = "us-east-1/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
--- aws_workloads/terraform.tfvars
+++
@@ -1,9 +0,0 @@
-aws_region = "us-east-1"
-
-instance_type = "t2.small"
-
-server_port = "8080"
-
-image_id = "ami-00874d747dde814fa"
-
-alb_port = "80"
