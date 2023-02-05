74bdff3984e4666530be29ef989e184fee3d5d7e
padfoot.black <padfoot@cybertron.io>
2023-02-04 19:33:32 -0500

    adding variables, updating resource names

--- terraform.tfvars
+++ terraform.tfvars
@@ -3,3 +3,7 @@ aws_region = "eu-west-1"
 instance_type = "t2.small"
 
 server_port = "8080"
+
+image_id    = "ami-084237e82d7842286"
+
+alb_port    =   "80"
--- variables.tf
+++ variables.tf
@@ -10,3 +10,14 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
 }
+
+variable "image_id" {
+  description = "Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type"
+  type = string
+ }
+
+ variable "alb_port" {
+  description = "Listener port for ALB"
+  type = string
+   
+ }
