a593b104e06722c929781a77444409fa4d8920b2
padfoot.black <padfoot@cybertron.io>
2023-02-05 17:19:15 -0500

    updating root module to reference tagged version of compute module

--- aws_live_workloads/main.tf
+++ aws_live_workloads/main.tf
@@ -1,5 +1,5 @@
 module "webserver_cluster" {
-  source = "../modules/compute/"
+  source = "../modules/compute/?ref=v0.0.1"
 
   cluster_name = "webserver-stage"
   instance_type = var.instance_type
