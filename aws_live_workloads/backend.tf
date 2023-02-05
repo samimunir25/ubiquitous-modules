dc9405e1eb455a0a59655d04bdb013086ed5ce25
padfoot.black <padfoot@cybertron.io>
2023-02-05 13:10:01 -0500

    updating backend details for live workloads

--- aws_live_workloads/backend.tf
+++ aws_live_workloads/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "remote-state-065374699403"
-    key     = "us-east-1/terraform.tfstate"
+    bucket  = "ubiquitous-modules-terraform"
+    key     = "aws_live_workloads/us-east-1/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
