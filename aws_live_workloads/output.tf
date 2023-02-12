996037f8dc72f2ac903c6d2a30fb319c84a00ff0
padfoot.black <padfoot@cybertron.io>
2023-02-12 03:05:27 -0500

    (feb/12) - updating output block

--- aws_live_workloads/output.tf
+++ aws_live_workloads/output.tf
@@ -1,3 +1,8 @@
+output "vpc_id" {
+  value       = module.custom_vpc.this_vpc_id
+  description = "VPC ID"
+}
+
 output "alb_dns_name" {
   value       = module.webserver_cluster.alb_dns_name
   description = "The domain name of the load balancer"
