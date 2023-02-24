8af1d460f25ad80681312038f715c68a93372740
padfoot.black <padfoot@cybertron.io>
2023-02-24 00:31:57 -0500

    (feb/23) - updating output.tf

--- aws_live_workloads/output.tf
+++ aws_live_workloads/output.tf
@@ -3,6 +3,11 @@ output "vpc_id" {
   description = "VPC ID"
 }
 
+output "this_nat_gw_ip" {
+  value       = module.custom_vpc.this_nat_gw_ip
+  description = "Public IP of NAT Gateway"
+}
+
 output "alb_dns_name" {
   value       = module.webserver_cluster.alb_dns_name
   description = "The domain name of the load balancer"
